4632fa67e7e02dcc94f3ab13adfff864cba785b8
leezh <22004832@siswa.um.edu.my>
2024-05-25 22:43:31 +0800

    fe: basic structure of contact us

--- resources/views/contact-us.blade.php
+++ resources/views/contact-us.blade.php
@@ -3,5 +3,24 @@
 @section('title', 'Contact Us')
 
 @section('content')
-<div> contact us</div>
+<main>
+    <div class="container-fluid content">
+        <div class="row g-3 mb-5">
+            <div class="col-6">
+                <h2 class="mb-4">Contact Us</h2>
+                <p>We're here to help and answer any questions you might have. We looking forward to hearing from you!</p>
+                <p><i class="bi bi-geo-alt-fill me-3"></i>Hotel Address</p>
+                <p><i class="bi bi-telephone-fill me-3"></i>03 2142 8000</p>
+                <p><i class="bi bi-envelope-fill me-3"></i>hotelname@gmail.com</p>
+            </div>
+            <div class="col-6"><div class="temp-image"></div></div>
+        </div>
+        <div class="row g-3 mb-3">
+            <div class="col-6">
+                <h4 class="mb-3">Rate Us</h4>
+                <div data-coreui-toggle="rating" data-coreui-value="3"></div>
+            </div>
+        </div>
+    </div>
+</main>
 @endsection
